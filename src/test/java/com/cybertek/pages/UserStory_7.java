07a535d8ed00cce2ce66231b0311a95df7c1d575
Xapreezy <xapreezy@Gmail.com>
2019-02-02 17:57:24 -0500

    added user story 7

--- src/test/java/com/cybertek/pages/UserStory_7.java
+++ src/test/java/com/cybertek/pages/UserStory_7.java
@@ -1,4 +1,21 @@
 package com.cybertek.pages;
 
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class UserStory_7 {
+    public UserStory_7() {
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+    @FindBy(xpath = "/html/body/div[1]/div[2]/div[2]/div/div/div/table/tbody/tr[1]/td[1]/div/input")
+    public WebElement customer1;
+
+    @FindBy(xpath = "/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/div/div[1]/button")
+    public WebElement PrintBar;
+    @FindBy(xpath = "/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/div/div[2]/button")
+    public WebElement ActionBar;
+
 }
--- src/test/java/com/cybertek/tests/UserStory_7_Test.java
+++ src/test/java/com/cybertek/tests/UserStory_7_Test.java
@@ -1,4 +1,29 @@
 package com.cybertek.tests;
 
-public class UserStory_7_Test {
+
+import com.cybertek.pages.UserStory_7;
+import com.cybertek.utilities.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserStory_7_Test extends TestBase {
+
+
+
+    public UserStory_7 userStory_1;
+    UserStory_7 userStory7;
+
+    @Test
+    public void ShouldDisplay() {
+
+        userStory7 = new UserStory_7();
+
+        if (userStory7.customer1.isDisplayed()) {
+            userStory7.customer1.click();
+        }
+        Assert.assertTrue(userStory7.PrintBar.isDisplayed());
+        Assert.assertTrue(userStory7.ActionBar.isDisplayed());
+
+    }
+
 }
