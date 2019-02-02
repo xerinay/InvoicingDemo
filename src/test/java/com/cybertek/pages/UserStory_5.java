8a9cb4741c20d4205519ffed4bdef955a85ce4f5
Shernay <xerinay85@gmail.com>
2019-02-02 18:25:25 -0500

    added first user story

--- src/test/java/com/cybertek/pages/UserStory_5.java
+++ src/test/java/com/cybertek/pages/UserStory_5.java
@@ -1,4 +1,18 @@
 package com.cybertek.pages;
 
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class UserStory_5 {
+    public UserStory_5() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//input[@placeholder='Search...']")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "//tbody[@class='ui-sortable']//tr[1]//td[2]")
+    public WebElement clickOnName;
 }
--- src/test/java/com/cybertek/tests/UserStory_5_Test.java
+++ src/test/java/com/cybertek/tests/UserStory_5_Test.java
@@ -1,4 +1,25 @@
 package com.cybertek.tests;
 
+import com.cybertek.pages.UserStory_5;
+import com.cybertek.utilities.BrowserUtils;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
 public class UserStory_5_Test {
+    public UserStory_5 userStory_5;
+
+    @Test
+    public void searchingName(){
+        userStory_5=new UserStory_5();
+
+
+        BrowserUtils.wait(3);
+        userStory_5.searchBox.sendKeys("Agrolait" + Keys.ENTER);
+        BrowserUtils.wait(3);
+
+        userStory_5.clickOnName.click();
+        BrowserUtils.wait(3);
+
+
+    }
 }
